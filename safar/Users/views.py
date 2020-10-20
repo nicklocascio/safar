b900c2f544839fa6415a133acf29120dd48b7e12
Nicholas Locascio <nicklocascio@rainmaker.local>
2020-10-19 20:21:12 -0400

    Added html pages and updated settings and views for account creation/display

--- safar/Users/views.py
+++ safar/Users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def display_user(request):
+    users = User.objects.all()
+    return render(request, 'display_users.hmtl', {'users': users})
+
+def signup(request):
+    form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
