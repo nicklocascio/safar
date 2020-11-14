91001165b474b21d5480c944e1c3f64ad9105d4f
vgoyette <vgoyette@nd.edu>
2020-11-14 02:51:27 -0500

    Styling and Routing. Beefed up the about page

--- safar/about/views.py
+++ safar/about/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def about(request):
-    return HttpResponse('About Page')
+    return render(request, 'about_custom.html')
 
 def home(request):
     return render(request, 'index.html')
